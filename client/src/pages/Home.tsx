3c9fe1ec8277436ac921a0e2745e5b19a533b2c9
scott|y|_[702] <ceo@civiveunlimited.com>
2026-04-03 15:14:16 -0700

    Repair homepage spy modal wiring

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
+import CompetitorSpyModal from "@/components/CompetitorSpyModal";
 import ContactSection from "@/components/sections/ContactSection";
 import FAQSection from "@/components/sections/FAQSection";
 import HeroSection from "@/components/sections/HeroSection";
 import ResultsSection from "@/components/sections/ResultsSection";
 
 export default function Home() {
+  const [isSpyModalOpen, setIsSpyModalOpen] = useState(false);
+
   return (
     <>
-      <HeroSection />
+      <HeroSection onOpenSpyModal={() => setIsSpyModalOpen(true)} />
       <ResultsSection />
       <FAQSection />
       <ContactSection />
+      <CompetitorSpyModal
+        isOpen={isSpyModalOpen}
+        onClose={() => setIsSpyModalOpen(false)}
+      />
     </>
   );
 }
