dca7113ec722eaf7feee67d3213a9b5262b4ff5b
scott|y|_[702] <ceo@civiveunlimited.com>
2026-04-03 15:10:50 -0700

    Simplify homepage sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,37 +1,15 @@
-import { useState } from "react";
-import HeroSection from "@/components/sections/HeroSection";
-import InfrastructureSection from "@/components/sections/InfrastructureSection";
-import GHLShowcaseSection from "@/components/sections/GHLShowcaseSection";
-import AISearchSection from "@/components/sections/AISearchSection";
-import ServicesSection from "@/components/sections/ServicesSection";
-import PricingSection from "@/components/sections/PricingSection";
-import StorySection from "@/components/sections/StorySection";
-import ResultsSection from "@/components/sections/ResultsSection";
-import ProcessSection from "@/components/sections/ProcessSection";
-import FAQSection from "@/components/sections/FAQSection";
 import ContactSection from "@/components/sections/ContactSection";
-import CompetitorSpyModal from "@/components/CompetitorSpyModal";
+import FAQSection from "@/components/sections/FAQSection";
+import HeroSection from "@/components/sections/HeroSection";
+import ResultsSection from "@/components/sections/ResultsSection";
 
 export default function Home() {
-  const [isSpyModalOpen, setIsSpyModalOpen] = useState(false);
-
   return (
     <>
-      <HeroSection onOpenSpyModal={() => setIsSpyModalOpen(true)} />
-      <InfrastructureSection />
-      <ServicesSection />
-      <GHLShowcaseSection />
-      <AISearchSection />
-      <PricingSection />
+      <HeroSection />
       <ResultsSection />
-      <ProcessSection />
-      <StorySection />
       <FAQSection />
       <ContactSection />
-      <CompetitorSpyModal
-        isOpen={isSpyModalOpen}
-        onClose={() => setIsSpyModalOpen(false)}
-      />
     </>
   );
 }
